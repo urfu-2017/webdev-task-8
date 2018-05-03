827cf2e58e4e98909913885f75c081ae3ce8de45
Morkhod <albert.voiceinyou@gmail.com>
2018-05-04 02:09:42 +0500

    hru

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get('/', (req, res) =>
     res.sendFile('./index.html')
 );
 
-app.listen(3000, () => {
-    console.info('Opened in localhost:3000');
+app.listen(8080, () => {
+    console.info('Opened in localhost:8080');
 });
 
 module.exports = app;
