6189c8c7715ad2a19def9b5e1de5d31f49a2e3df
Александр Белёв <belyov97@gmail.com>
2018-05-16 13:39:37 +0500

    sleeping and notifications work

--- js/index.js
+++ js/index.js
@@ -48,6 +48,7 @@ class Game {
                 console.log(battery);
                 if (battery.charging) {
                     HRUN._action = 'eating';
+                    stopListening();
                 } else {
                     HRUN._action = 'rest';
                 }
@@ -59,20 +60,12 @@ class Game {
         HRUN = HrundelState.initMax();
     }
 
-    rerender(hrundel) {
-        document.querySelector('#satietyValue').innerHTML = hrundel.satiety;
-        document.querySelector('#energyValue').innerHTML = hrundel.energy;
-        document.querySelector('#moodValue').innerHTML = hrundel.mood;
-        console.log(hrundel.satiety);
-    }
-
 }
 
 function rerender(hrundel) {
     document.querySelector('#satietyValue').innerHTML = hrundel.satiety;
     document.querySelector('#energyValue').innerHTML = hrundel.energy;
     document.querySelector('#moodValue').innerHTML = hrundel.mood;
-    console.log(hrundel.satiety);
 }
 
 class HrundelState {
@@ -110,22 +103,57 @@ class HrundelState {
     }
 
     reduceState() {
+        console.log(this._action);
         switch (this._action) {
             case 'rest':
-                this.satiety--;
-                this.mood--;
-                this.energy--;
-                console.log('rest');
+                this.decreaseProp('satiety');
+                this.decreaseProp('mood');
+                this.decreaseProp('energy');
                 break;
             case 'eating':
-                this.satiety = Math.min(this.satiety + 3, 100);
-                this.mood--;
-                this.energy--;
-                console.log('eating');
+                this.increaseProp('satiety', 3);
+                this.decreaseProp('mood');
+                this.decreaseProp('eating');
+                break;
+            case 'communicating':
+                this.increaseProp('mood', 3);
+                this.decreaseProp('satiety');
+                this.decreaseProp('energy');
+                if (this.mood === 100) {
+                    stopListening();
+                    this._action = 'rest';
+                }
+                break;
+            case 'sleeping':
+                this.decreaseProp('mood');
+                this.decreaseProp('satiety');
+                this.increaseProp('energy', 3);
+                if (this.energy === 100) {
+                    this._action = 'rest';
+                }
+                break;
+
+        }
+
+        if (!isActive) {
+            if (this.satiety === 9) {
+                createNotification('Я голодный :(');
+            }
+            if (this.mood === 9) {
+                createNotification('Мне грустно :с');
+            }
         }
 
         rerender(this);
         setTimeout(this.reduceState.bind(HRUN), 1000);
+    }
+
+    increaseProp(name, value = 1) {
+        this[name] = Math.min(this[name] + value, 100);
+    }
+
+    decreaseProp(name, value = 1) {
+        this[name] = Math.max(this[name] - value, 0);
     }
 
     set action(value) {
@@ -142,6 +170,97 @@ class HrundelState {
 }
 
 let HRUN = HrundelState.loadFromStorage() || HrundelState.initMax();
+let LISTENER;
+let NOTIFICATION;
+let isActive = true;
+
+function initListening() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!SpeechRecognition)
+        return;
+    LISTENER = new SpeechRecognition();
+    LISTENER.lang = 'ru-RU';
+    LISTENER.continious = true;
+
+    LISTENER.onerror = err => {
+        console.error(err);
+        HRUN._action='rest';
+    };
+
+    LISTENER.onstart = function(e) {
+        console.log('started listening');
+        HRUN._action = 'communicating';
+    };
+
+    LISTENER.onstop = function(e) {
+        console.log('stopped listening');
+    };
+
+    LISTENER.onresult = function (e) {
+        const index = e.resultIndex;
+        const result = e.results[index][0].transcript.trim();
+
+        const speech = document.querySelector('#speech');
+        const oldResult = speech.innerText;
+        speech.innerText = oldResult + result;
+    };
+
+    document.querySelector('.hrun').onclick = function (e) {
+        console.log('Started listening');
+        LISTENER.start();
+    }
+}
+
+function stopListening() {
+    LISTENER.stop();
+    HRUN._action='rest';
+}
+
+function initSleeping() {
+    let hidden, visibilityChange;
+    if (typeof document.hidden !== "undefined") {
+        hidden = "hidden";
+        visibilityChange = "visibilitychange";
+    } else if (typeof document.msHidden !== "undefined") {
+        hidden = "msHidden";
+        visibilityChange = "msvisibilitychange";
+    } else if (typeof document.webkitHidden !== "undefined") {
+        hidden = "webkitHidden";
+        visibilityChange = "webkitvisibilitychange";
+    }
+
+    document.addEventListener(visibilityChange, function (e) {
+        if (document[hidden]) {
+            isActive = false;
+            HRUN._action = 'sleeping';
+        } else {
+            isActive = true;
+            HRUN._action = 'rest';
+        }
+    });
+}
+
+function initNotifications() {
+    const Notification = window.Notification || window.webkitNotification;
+    if (!Notification) {
+        console.error('Notifications not supported');
+    }
+
+    Notification.requestPermission(initSleeping);
+    NOTIFICATION = Notification;
+}
+
+function createNotification(text) {
+    const notification = new NOTIFICATION('Хрюндель', {
+        body: text
+    });
+    notification.onerror = function (err) {
+        console.error(err);
+    };
+    notification.onshow = function (e) {
+        console.log('shown');
+    };
+}
 
 
 window.onload = () => {
@@ -149,10 +268,11 @@ window.onload = () => {
     const game = new Game(HRUN);
     game.reset();
     game.initFeed();
-    // const hrun=HrundelState.loadFromStorage()||HrundelState.initMax();
     console.log(HRUN.satiety, HRUN.energy, HRUN.mood);
     rerender(HRUN);
     HRUN.reduceState();
+    initListening();
+    initNotifications();
 };
 
 window.onunload = () => {
